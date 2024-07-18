b8c2de50c04a4c08f815be2731280184cb94eee7
Sandeepcse48 <gsreddy548@gmail.com>
2024-07-18 20:37:39 +0530

    Changes

--- src/JavaProgramming/TestingDebug.java
+++ src/JavaProgramming/TestingDebug.java
@@ -1,13 +1,23 @@
 package JavaProgramming;
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 
 public class TestingDebug {
 
+	final int a = 20;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+        TestingDebug td = new TestingDebug();
+		td.check(20);
+		
 		System.out.println("git - global information tracker");
+	}
+	public void check(int a) {
+		a = this.a;
+		System.out.println(a);
 	}
 
 }
